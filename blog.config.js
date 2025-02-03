b81cf646793abc16ab484fb809cd33b68e23db9b
xb1224 <149314867+xb1224@users.noreply.github.com>
2025-02-03 19:02:03 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -52,6 +52,8 @@ const BLOG = {
   // 自定义菜单
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
 
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true,
+
   // 文章列表相关设置
   CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
 
